26f75e8441bc0f4fbad5d50779bb75aee66e729b
AndrewKostuk <andrkost78@gmail.com>
2022-07-25 21:22:25 +0300

    initial project add

---
+++ src/test/java/by/bsuir/andrei/petclinic/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.andrei.petclinic.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
