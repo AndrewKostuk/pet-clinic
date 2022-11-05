1cb5407762f1f569ff07129d16ac6529b17e1d71
AndrewKostuk <andrkost78@gmail.com>
2022-11-05 17:58:25 +0300

    implement map based services. closes #6

---
+++ pet-clinic-data/src/main/java/by/bsuir/andrei/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package by.bsuir.andrei.petclinic.services.map;
+
+import by.bsuir.andrei.petclinic.model.Vet;
+import by.bsuir.andrei.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+}
