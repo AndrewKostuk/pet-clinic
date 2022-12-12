735452616576202599dd2a685cd16fa3687bf4d1
AndrewKostuk <andrkost78@gmail.com>
2022-12-12 22:27:02 +0300

    change owner mappings

--- pet-clinic-web/src/main/java/by/bsuir/andrei/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/by/bsuir/andrei/petclinic/controllers/OwnerController.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("owners")
+@RequestMapping("/owners")
 public class OwnerController {
 
-    @RequestMapping({"", "/", "index", "index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String listOwners() {
         return "owners/index";
     }
