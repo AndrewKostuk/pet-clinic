c9836dd6d7b3ae2676e5d90256b8adf7e736763f
AndrewKostuk <andrkost78@gmail.com>
2023-02-25 21:36:10 +0300

    refactor map implementation to control the id property; closed #23

--- pet-clinic-web/src/main/java/by/bsuir/andrei/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/by/bsuir/andrei/petclinic/bootstrap/DataLoader.java
@@ -21,13 +21,11 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("ao");
         owner1.setLastName("aao");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("bo");
         owner2.setLastName("bbo");
         ownerService.save(owner2);
@@ -35,13 +33,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("new owners have been saved");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("av");
         vet1.setLastName("aav");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("bv");
         vet2.setLastName("bbv");
         vetService.save(vet2);
