7fecc234e2bdc8286e6176a0b2f8ed59e8e1ba5c
AndrewKostuk <andrkost78@gmail.com>
2023-07-04 18:44:19 +0300

    add petType service implementation; close #28

---
+++ pet-clinic-data/src/main/java/by/bsuir/andrei/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package by.bsuir.andrei.petclinic.services;
+
+import by.bsuir.andrei.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/by/bsuir/andrei/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package by.bsuir.andrei.petclinic.services.map;
+
+import by.bsuir.andrei.petclinic.model.PetType;
+import by.bsuir.andrei.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
